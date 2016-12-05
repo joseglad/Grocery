4b5596ff4500237f14aa0a46ba7b682366550fcf
Pansa Jose <pansa@b10p19.fil.univ-lille1.fr>
2016-12-05 12:48:53 +0100

    first commit

---
+++ src/com/jose/beans/Article.java
@@ -0,0 +1,32 @@
+package com.jose.beans;
+
+public class Article {
+	private int id;
+	private String nom;
+	private Double price;
+	
+	
+	public Article() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+}
